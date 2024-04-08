853c4f54b6c8b3a040154928a771599172df8c8e
antheas <antheas@users.noreply.github.com>
2024-04-08 21:53:14 +0000

    update to ''

--- hhd-ui.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-export ELECTRON_IS_DEV=0
-cd /usr/lib/hhd-ui
-# --no-sandbox is required for it to launch within steam
-exec electron@electronversion@ --no-sandbox /usr/lib/hhd-ui/app.asar $@
